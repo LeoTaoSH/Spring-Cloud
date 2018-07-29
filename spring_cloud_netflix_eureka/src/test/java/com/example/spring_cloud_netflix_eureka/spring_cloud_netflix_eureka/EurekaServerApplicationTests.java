4c127c9f3413877bbfcee46d4912e1706e3b9786
Leo Tao <taoxb@cn.ibm.com>
2018-07-29 17:13:57 +0800

    added config projects

---
+++ spring_cloud_netflix_eureka/src/test/java/com/example/spring_cloud_netflix_eureka/spring_cloud_netflix_eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.spring_cloud_netflix_eureka.spring_cloud_netflix_eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
